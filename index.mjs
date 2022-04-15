014c78fb363c708af92ba51193e5a2fbb8958354
Angel Psiakis <angel@mml.rocks>
2022-04-15 09:50:58 +0100

    big bang

---
+++ index.mjs
@@ -0,0 +1,22 @@
+import { loadStdlib } from '@reach-sh/stdlib';
+import * as backend from './build/index.main.mjs';
+
+const stdlib = loadStdlib(process.env);
+
+(async () => {
+  const startingBalance = stdlib.parseCurrency(10);
+  const accAlice = await stdlib.newTestAccount(startingBalance);
+  const accBob = await stdlib.newTestAccount(startingBalance);
+
+  const ctcAlice = accAlice.deploy(backend)
+  const ctcBob = accBob.attach(backend, ctcAlice.getInfo())
+
+  await Promise.all([
+    backend.Alice(ctcAlice, {
+
+    }),
+
+    backend.Bob(ctcBob, {
+    }),
+  ]);
+})();
---
+++ index.rsh
@@ -0,0 +1,13 @@
+'reach 0.1'
+
+export const main = Reach.App(() => {
+  const Alice = Participant('Alice', {
+    // Alice's code
+  });
+
+  const Bob = Participant('Bob', {
+    // Bob's code
+  });
+
+  init();
+});

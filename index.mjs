bf0edb05ca3c57d48162ba225527d80ec73dfeb1
Angel Psiakis <angel@mml.rocks>
2022-04-15 15:09:14 +0100

    it runs

--- index.mjs
+++ index.mjs
@@ -1,5 +1,5 @@
-import { loadStdlib } from '@reach-sh/stdlib';
-import * as backend from './build/index.main.mjs';
+import { loadStdlib } from "@reach-sh/stdlib";
+import * as backend from "./build/index.main.mjs";
 
 const stdlib = loadStdlib(process.env);
 
@@ -8,15 +8,29 @@ const stdlib = loadStdlib(process.env);
   const accAlice = await stdlib.newTestAccount(startingBalance);
   const accBob = await stdlib.newTestAccount(startingBalance);
 
-  const ctcAlice = accAlice.deploy(backend)
-  const ctcBob = accBob.attach(backend, ctcAlice.getInfo())
+  const ctcAlice = accAlice.contract(backend);
+  const ctcBob = accBob.contract(backend, ctcAlice.getInfo());
 
+  const HAND = ["Rock", "Paper", "Scissors"];
+  const OUTCOME = ["Bob wins", "Draw", "Alice wins"];
+
+  const Player = (Who) => ({
+    getHand: () => {
+      const hand = Math.floor(Math.random() * HAND.length);
+      console.log(`${Who} played ${HAND[hand]}`);
+      return hand;
+    },
+    seeOutcome: (outcome) => {
+      console.log(`${Who} saw outcome ${OUTCOME[outcome]}`);
+    },
+  });
   await Promise.all([
     backend.Alice(ctcAlice, {
-
+      ...Player("Alice"),
     }),
 
     backend.Bob(ctcBob, {
+      ...Player("Bob"),
     }),
   ]);
 })();
